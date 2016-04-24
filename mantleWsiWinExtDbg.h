9122c5c06eafc7df063fca5549dfe837ec840c74
Igor <tesla707@mail.ru>
2016-04-24 17:57:37 +0300

    Create mantleWsiWinExtDbg.h

---
+++ mantleWsiWinExtDbg.h
@@ -0,0 +1,16 @@
+#pragma once
+#ifndef MANTLEWSIWINEXTDBG_H_
+#define MANTLEWSIWINEXTDBG_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// something ???
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
