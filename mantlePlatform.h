c02d2700e1f421783abaf61eb82292f49b6bec2d
tesla707 <tesla707@mail.ru>
2019-12-03 17:25:44 +0300

    Update mantlePlatform.h

--- mantlePlatform.h
+++ mantlePlatform.h
@@ -1,10 +1,23 @@
-#pragma once
+/*
+
+Module Name:
+
+	mantlePlatform.h
+
+Abstract:
+
+	Platform specific definitions
+
+*/
+
+
 #ifndef MANTLEPLATFORM_H_
 #define MANTLEPLATFORM_H_
 
+
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif // __cplusplus
 
 #if defined(_WIN32)
 #define GR_STDCALL __stdcall
@@ -15,36 +28,36 @@ extern "C" {
 #endif
 
 #include <stddef.h>
-#include <Windows.h>
 
 #if !defined(GR_NO_STDINT_H)
 #if defined(_MSC_VER) && (_MSC_VER < 1600)
 	typedef unsigned char GR_BYTE;
 	typedef char GR_CHAR;
 	typedef float GR_FLOAT;
-	typedef unsigned char GR_UINT8;
+	typedef signed __int32 GR_BOOL;
+	typedef signed __int8 GR_INT8;
+	typedef unsigned __int8 GR_UINT8;
 	typedef signed int GR_INT;
-	typedef signed __int32 GR_INT32;
 	typedef unsigned int GR_UINT;
+	typedef signed __int32 GR_INT32;
 	typedef unsigned __int32 GR_UINT32;
-	typedef unsigned __int64 GR_UINT64;
+	typedef signed __int64 GR_INT64;
+	typedef  unsigned __int64 GR_UINT64;
 	typedef unsigned long long GR_SIZE;
 	typedef unsigned long long GR_GPU_SIZE;
-	typedef signed int GR_ENUM;
-	typedef void GR_VOID;
-	typedef unsigned int GR_FLAGS;
-	typedef unsigned int GR_BOOL;
-	typedef unsigned __int32 GR_SAMPLE_MASK;
+	typedef signed __int32 GR_ENUM;
+	typedef signed __int32 GR_FLAGS;
+	typedef unsigned long GR_SAMPLE_MASK;
 #else
 #include <stdint.h>
-	typedef BYTE GR_BYTE;
+	typedef uint8_t GR_BYTE;
 	typedef char GR_CHAR;
 	typedef float GR_FLOAT;
-	typedef BOOL GR_BOOL;
+	typedef int32_t GR_BOOL;
 	typedef int8_t GR_INT8;
 	typedef uint8_t GR_UINT8;
-	typedef INT GR_INT;
-	typedef UINT GR_UINT;
+	typedef int32_t GR_INT;
+	typedef uint32_t GR_UINT;
 	typedef int32_t GR_INT32;
 	typedef uint32_t GR_UINT32;
 	typedef int64_t GR_INT64;
@@ -55,9 +68,38 @@ extern "C" {
 	typedef int32_t GR_FLAGS;
 	typedef uint32_t GR_SAMPLE_MASK;
 #endif
+
+#endif // !defined(GR_NO_STDINT_H)
+
+
+#ifdef GR_USE_PLATFORM_WAYLAND
+#include <wayland-client.h>
 #endif
 
+#ifdef GR_USE_PLATFORM_WIN32
+#include <windows.h>
+#endif
+
+#ifdef GR_USE_PLATFORM_XCB
+#include <xcb/xcb.h>
+#endif
+
+#ifdef GR_USE_PLATFORM_XLIB
+#include <X11/Xlib.h>
+#endif
+
+#ifdef GR_USE_PLATFORM_XLIB_XRANDR_EXT
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+#endif
+
+#ifdef GR_USE_PLATFORM_GGP
+#include <ggp_c/vulkan_types.h>
+#endif
+
+
 #ifdef __cplusplus
-}
-#endif
-#endif
+} // extern "C"
+#endif // __cplusplus
+
+#endif // MANTLEPLATFORM_H_
